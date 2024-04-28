7e144ee9f598e1d9aff3fd094a4587de03cc9ab4
Designolle <sdesigno@htc-login001.zib.de>
2024-04-28 20:32:11 +0200

    Add tests for MUBs

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,13 @@
-using Ket
-using Test
+using CyclotomicNumbers
 using DoubleFloats
+using Ket
 using Quadmath
+using Test
 
-tests = ["norms.jl"]
+tests = ["measurements.jl", "norms.jl"]
 
+# SD: I'm no big fan of this for solution. I understand the idea, but it makes it hard to comment out some files when testing a specific set.
+# SD: Unless someone has a strong opinion about this, I'll change that in the future.
 for test in tests
-    include(test);
+    include(test)
 end
