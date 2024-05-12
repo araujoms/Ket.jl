29693b3e0a4d3ec00dd00bee62d077f81b13f5e3
Sébastien Designolle <sebastien.designolle@gmail.com>
2024-05-12 20:22:57 +0200

    Edit signatures and docstrings (type convention)

--- src/random.jl
+++ src/random.jl
@@ -1,5 +1,5 @@
 """
-    random_state(d::Integer, k::Integer=d)
+    random_state([T=ComplexF64,] d::Integer, k::Integer = d)
 
 Produces a uniformly distributed random quantum state in dimension `d` with rank `k`.
 
@@ -15,7 +15,7 @@ random_state(d::Integer, k::Integer = d) = random_state(ComplexF64, d, k)
 export random_state
 
 """
-    random_state_vector(d::Integer)
+    random_state_vector([T=ComplexF64,] d::Integer)
 
 Produces a Haar-random quantum state vector in dimension `d`.
 
@@ -30,14 +30,14 @@ random_state_vector(d::Integer) = random_state_vector(ComplexF64, d)
 export random_state_vector
 
 """
-    random_unitary(d::Integer; T::Type, R::Type = Complex{T})
+    random_unitary([T=ComplexF64,] d::Integer)
 
 Produces a Haar-random unitary matrix in dimension `d`.
-If `R` is a real type the output is instead a Haar-random (real) orthogonal matrix.
+If `T` is a real type the output is instead a Haar-random (real) orthogonal matrix.
 
 Reference: Mezzadri, [arXiv:math-ph/0609050](https://arxiv.org/abs/math-ph/0609050).
 """
-function random_unitary(::Type{T}, d::Integer) where {T}
+function random_unitary(::Type{T}, d::Integer) where {T<:Number}
     z = randn(T, (d, d))
     Q, R = LA.qr(z)
     Λ = sign.(real(LA.Diagonal(R)))
@@ -47,28 +47,29 @@ random_unitary(d::Integer) = random_unitary(ComplexF64, d)
 export random_unitary
 
 """
-    random_povm(d::Integer, n::Integer, r::Integer)
+    random_povm([T=ComplexF64,] d::Integer, n::Integer, r::Integer)
 
 Produces a random POVM of dimension `d` with `n` outcomes and rank `min(k, d)`.
 
 Reference: Heinosaari et al., [arXiv:1902.04751](https://arxiv.org/abs/1902.04751).
 """
-function random_povm(d::Integer, n::Integer, k::Integer = d; T::Type = Float64, R::Type = Complex{T})
+function random_povm(::Type{T}, d::Integer, n::Integer, k::Integer = d) where {T<:Number}
     d ≤ n * k || throw(ArgumentError("We need d ≤ n*k, but got d = $(d) and n*k = $(n*k)"))
-    E = [Matrix{R}(undef, (d, d)) for _ = 1:n]
+    E = [Matrix{T}(undef, (d, d)) for _ = 1:n]
     for i = 1:n
-        G = randn(R, (d, k))
+        G = randn(T, (d, k))
         LA.mul!(E[i], G, G')
     end
     S = sum(LA.Hermitian.(E))
     rootinvS = sqrt(inv(S)) #don't worry, the probability of getting a singular S is zero
-    mat = Matrix{R}(undef, (d, d))
+    mat = Matrix{T}(undef, (d, d))
     for i = 1:n
         LA.mul!(mat, rootinvS, E[i])
         LA.mul!(E[i], mat, rootinvS)
     end
     return LA.Hermitian.(E)
 end
+random_povm(d::Integer, n::Integer, k::Integer = d) = random_povm(ComplexF64, d, n, k)
 export random_povm
 
 """
