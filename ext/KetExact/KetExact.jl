4011f72e77a82f3a2b42ceb7798babaa808789dd
Sébastien Designolle <sebastien.designolle@gmail.com>
2024-07-04 17:44:15 +0200

    Add TExact

--- ext/KetExact/KetExact.jl
+++ ext/KetExact/KetExact.jl
@@ -4,10 +4,13 @@ import CyclotomicNumbers as CN
 import Ket
 import LinearAlgebra as LA
 
+const TExact = Cyc{Rational{BigInt}}
+export TExact
+
 Ket._root_unity(::Type{CN.Cyc{R}}, n::Integer) where {R<:Real} = CN.E(Int64(n))
 Ket._sqrt(::Type{CN.Cyc{R}}, n::Integer) where {R<:Real} = CN.root(Int64(n))
 Ket._rtol(::Type{CN.Cyc{R}}) where {R<:Real} = Base.rtoldefault(R)
-Ket._eps(::Type{CN.Cyc{R}}) where {R<:Real} = 0
+Ket._eps(::Type{CN.Cyc{R}}) where {R<:Real} = R(0)
 Ket._eps(::Type{CN.Cyc{R}}) where {R<:AbstractFloat} = eps(R)
 
 LA.norm(v::AbstractVector{CN.Cyc{R}}) where {R<:Real} = Ket._sqrt(CN.Cyc{R}, Int64(sum(abs2, v)))
