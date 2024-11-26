ccc0a570459c1563f998341dc2208d15b99562cd
Sébastien Designolle <sebastien.designolle@gmail.com>
2024-11-27 00:03:02 +0100

    Add conversion tests for more types

--- test/nonlocal.jl
+++ test/nonlocal.jl
@@ -16,34 +16,38 @@
 end
 
 @testset "FP and FC notations" begin
-    Aax = povm(mub(2))
-    fc_phiplus = Diagonal([1, 1, 1, -1])
-    @test tensor_correlation(state_phiplus(), Aax, 2) ≈ fc_phiplus
-    @test tensor_correlation(state_phiplus(), Aax, 2; marg = false) ≈ fc_phiplus[2:end, 2:end]
-    fc_ghz = zeros(4, 4, 4)
-    fc_ghz[[1, 6, 18, 21, 43, 48, 60, 63]] .= [1, 1, 1, 1, 1, -1, -1, -1]
-    @test tensor_correlation(state_ghz(), Aax, 3) ≈ fc_ghz
-    @test tensor_correlation(state_ghz(), Aax, 3; marg = false) ≈ fc_ghz[2:end, 2:end, 2:end]
-    m = [3, 4, 5] # dichotomic outcomes
-    fc1 = randn(m...)
-    fc2 = randn(m...)
-    fp1 = tensor_probability(fc1, true)
-    fp2 = tensor_probability(fc2, false)
-    @test tensor_correlation(fp1, true) ≈ fc1
-    @test tensor_correlation(fp2, false) ≈ fc2
-    @test dot(fc1, fc2) ≈ dot(fp1, fp2)
+    for T in [Float64, Double64, Float128, BigFloat]
+        Aax = povm(mub(Complex{T}, 2))
+        fc_phiplus = Diagonal([1, 1, 1, -1])
+        @test tensor_correlation(state_phiplus(Complex{T}), Aax, 2) ≈ fc_phiplus
+        @test tensor_correlation(state_phiplus(Complex{T}), Aax, 2; marg = false) ≈ fc_phiplus[2:end, 2:end]
+        fc_ghz = zeros(Int64, 4, 4, 4)
+        fc_ghz[[1, 6, 18, 21, 43, 48, 60, 63]] .= [1, 1, 1, 1, 1, -1, -1, -1]
+        @test tensor_correlation(state_ghz(Complex{T}), Aax, 3) ≈ fc_ghz
+        @test tensor_correlation(state_ghz(Complex{T}), Aax, 3; marg = false) ≈ fc_ghz[2:end, 2:end, 2:end]
+        m = [3, 4, 5] # dichotomic outcomes
+        fc1 = randn(T, m...)
+        fc2 = randn(T, m...)
+        fp1 = tensor_probability(fc1, true)
+        fp2 = tensor_probability(fc2, false)
+        @test tensor_correlation(fp1, true) ≈ fc1
+        @test tensor_correlation(fp2, false) ≈ fc2
+        @test dot(fc1, fc2) ≈ dot(fp1, fp2)
+    end
 end
 
 @testset "FP and CG notations" begin
-    Aax = povm(mub(2))
-    cg_phiplus = [1.0 0.5 0.5 0.5; 0.5 0.5 0.25 0.25; 0.5 0.25 0.5 0.25; 0.5 0.25 0.25 0.0]
-    @test tensor_collinsgisin(state_phiplus(), Aax, 2) ≈ cg_phiplus
-    scenario = [2, 3, 4, 5]
-    cg1 = randn(scenario[3] * (scenario[1] - 1) + 1, scenario[4] * (scenario[2] - 1) + 1)
-    cg2 = randn(scenario[3] * (scenario[1] - 1) + 1, scenario[4] * (scenario[2] - 1) + 1)
-    fp1 = tensor_probability(cg1, scenario, true)
-    fp2 = tensor_probability(cg2, scenario, false)
-    @test tensor_collinsgisin(fp1, true) ≈ cg1
-    @test tensor_collinsgisin(fp2, false) ≈ cg2
-    @test dot(fp1, fp2) ≈ dot(cg1, cg2)
+    for T in [Float64, Double64, Float128, BigFloat]
+        Aax = povm(mub(Complex{T}, 2))
+        cg_phiplus = [1.0 0.5 0.5 0.5; 0.5 0.5 0.25 0.25; 0.5 0.25 0.5 0.25; 0.5 0.25 0.25 0.0]
+        @test tensor_collinsgisin(state_phiplus(Complex{T}, ), Aax, 2) ≈ cg_phiplus
+        scenario = [2, 3, 4, 5]
+        cg1 = randn(T, scenario[3] * (scenario[1] - 1) + 1, scenario[4] * (scenario[2] - 1) + 1)
+        cg2 = randn(T, scenario[3] * (scenario[1] - 1) + 1, scenario[4] * (scenario[2] - 1) + 1)
+        fp1 = tensor_probability(cg1, scenario, true)
+        fp2 = tensor_probability(cg2, scenario, false)
+        @test tensor_collinsgisin(fp1, true) ≈ cg1
+        @test tensor_collinsgisin(fp2, false) ≈ cg2
+        @test dot(fp1, fp2) ≈ dot(cg1, cg2)
+    end
 end
