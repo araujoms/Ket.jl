e8f9aede9a230cbff1cd969fa2dea6fb37dc3202
Sébastien Designolle <sebastien.designolle@gmail.com>
2024-11-26 15:13:32 +0100

    More concise syntax

--- src/nonlocal.jl
+++ src/nonlocal.jl
@@ -261,13 +261,13 @@ function tensor_correlation(p::AbstractArray{T, N2}, behaviour::Bool = true; mar
     cia = CartesianIndices(Tuple(2ones(Int64, N)))
     cix = CartesianIndices(size_output)
     for x in cix
-        x_colon = Union{Colon, Int64}[x[n] - marg == 0 ? Colon() : x[n] - marg for n in 1:N]
+        x_colon = Union{Colon, Int64}[x[n] > marg ? x[n] - marg : Colon() for n in 1:N]
         if ~behaviour
             error("Not implemented yet")
         else
             res[x] =
-                sum((-1)^sum(a[n] for n in 1:N if x[n] - marg > 0; init = 0) * sum(p[a, x_colon...]) for a in cia) /
-                prod(m[n] for n in 1:N if x[n] - marg == 0; init = 1)
+                sum((-1)^sum(a[n] for n in 1:N if x[n] > marg; init = 0) * sum(p[a, x_colon...]) for a in cia) /
+                prod(m[n] for n in 1:N if x[n] == marg; init = 1)
         end
         if abs2(res[x]) < _eps(T)
             res[x] = 0
