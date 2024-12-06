adea0f8231aad9e39f01a7f1e9151125b2f4c962
araujoms <maltusan@gmail.com>
2024-12-06 23:28:48 +0100

    really fix tests

--- test/states.jl
+++ test/states.jl
@@ -18,7 +18,7 @@
         coeff = [T(3) / 5, T(4) / 5]
         @test state_ghz_ket(T; coeff) == (T(3) * ket(1, 8) + T(4) * ket(8, 8)) / 5
         @test state_super_singlet(T, 2) ≈ state_psiminus(T)
-        U = foldl(kron, fill(random_unitary(T, 3), 3))
+        U = foldl(kron, fill(Matrix(random_unitary(T, 3)), 3))
         rho = state_super_singlet(T, 3)
         @test U * rho * U' ≈ rho
     end
