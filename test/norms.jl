434d097105fc71248188018f13dcda0303324596
araujoms <maltusan@gmail.com>
2025-01-15 12:04:03 +0100

    now it's actually testing the higher precision types

--- test/norms.jl
+++ test/norms.jl
@@ -1,29 +1,33 @@
 @testset "Norms              " begin
     @testset "Square operators" begin
         for T ∈ [Float64, Double64, Float128, BigFloat]
-            X = Complex{T}.([1.34 5im 2.6; 5.2 4+2.1im 0.1; -4.1 0.9-2.2im 1im])
-            @test isapprox(schatten_norm(X, 1.32), 12.366965628920612)
-            @test isapprox(trace_norm(X), 15.34224630614291)
-            @test isapprox(schatten_norm(X, 2), 10.22133063744638)
-            @test isapprox(schatten_norm(X, Inf), 9.07815413613693)
-            @test isapprox(kyfan_norm(X, 2, 1.7), 10.46707268381498)
-            @test isapprox(kyfan_norm(X, 3), schatten_norm(X, 2))
-            @test isapprox(kyfan_norm(X, 1), schatten_norm(X, Inf))
-            @test isapprox(kyfan_norm(X, 3, 1), trace_norm(X))
+            X = Complex{T}.([1+2im 3+4im; 5+6im 7+8im])
+            svds = [√(2 * (51 + √T(2537))), √(2 * (51 - √T(2537)))]
+            @test trace_norm(X) ≈ sum(svds)
+            @test schatten_norm(X, 2) ≈ √T(204)
+            @test schatten_norm(X, Inf) ≈ svds[1]
+            p = T(ℯ)
+            @test schatten_norm(X, p) ≈ sum(svds .^ p)^(1 / p)
+            @test kyfan_norm(X, 2, p) ≈ schatten_norm(X, p)
+            @test kyfan_norm(X, 2) ≈ schatten_norm(X, 2)
+            @test kyfan_norm(X, 1) ≈ schatten_norm(X, Inf)
+            @test kyfan_norm(X, 2, 1) ≈ trace_norm(X)
         end
     end
 
     @testset "Rectangular operators" begin
         for T ∈ [Float64, Double64, Float128, BigFloat]
-            X = Complex{T}.([1.34 5im 2.6; 5.2 4+2.1im 0.1])
-            @test isapprox(schatten_norm(X, 2.43), 8.68672362793173)
-            @test isapprox(trace_norm(X), 11.8442700353432)
-            @test isapprox(schatten_norm(X, 2), 9.00086662494229)
-            @test isapprox(schatten_norm(X, Inf), 8.25368317983077)
-            @test isapprox(kyfan_norm(X, 2, 4.3), schatten_norm(X, 4.3))
-            @test isapprox(kyfan_norm(X, 2), schatten_norm(X, 2))
-            @test isapprox(kyfan_norm(X, 1), schatten_norm(X, Inf))
-            @test isapprox(kyfan_norm(X, 2, 1), trace_norm(X))
+            X = Complex{T}.([1+2im 3+4im; 5+6im 7+8im; 9+10im 11+12im])
+            svds = [√(325 + √T(104089)), √(325 - √T(104089))]
+            @test trace_norm(X) ≈ sum(svds)
+            @test schatten_norm(X, 2) ≈ √T(650)
+            @test schatten_norm(X, Inf) ≈ svds[1]
+            p = T(ℯ)
+            @test schatten_norm(X, p) ≈ sum(svds .^ p)^(1 / p)
+            @test kyfan_norm(X, 2, p) ≈ schatten_norm(X, p)
+            @test kyfan_norm(X, 2) ≈ schatten_norm(X, 2)
+            @test kyfan_norm(X, 1) ≈ schatten_norm(X, Inf)
+            @test kyfan_norm(X, 2, 1) ≈ trace_norm(X)
         end
     end
 end
