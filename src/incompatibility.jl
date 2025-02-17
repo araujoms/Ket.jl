65c8cd22fbd1d6d63558e495581c3b3f66f97768
Sébastien Designolle <sebastien.designolle@gmail.com>
2025-02-17 15:00:27 +0100

    Fix underscore escaping

--- src/incompatibility.jl
+++ src/incompatibility.jl
@@ -7,8 +7,8 @@
         solver = Hypatia.Optimizer{_solver_type(T)})
 
 Computes the incompatibility robustness of the measurements in the vector `A`.
-Depending on the noise model chosen, the second argument can be "depolarizing" (`{tr(Aₐ)I/d}ₐ`, where `d` is the dimension of the system), "random" (`{I/n}ₐ`, where `n` is the number of outcomes), "probabilistic" (`{pₐI}ₐ`, where `p` is a probability distribution), "jointly\_measurable", or "general" (default).
-Returns the parent POVM if `return\_parent = true`.
+Depending on the noise model chosen, the second argument can be "depolarizing" (`{tr(Aₐ)I/d}ₐ`, where `d` is the dimension of the system), "random" (`{I/n}ₐ`, where `n` is the number of outcomes), "probabilistic" (`{pₐI}ₐ`, where `p` is a probability distribution), "jointly\\_measurable", or "general" (default).
+Returns the parent POVM if `return_parent = true`.
 
 References:
 - Designolle, Farkas, Kaniewski, [arXiv:1906.00448](https://arxiv.org/abs/1906.00448) (for the different noise models)
